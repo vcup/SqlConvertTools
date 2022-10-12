e15b1543bfca0826040f2efaba6cea6cd428110b
miryo <miryo@vcup.moe>
2022-10-12 15:53:06 +0800

    ~

--- SqlConvertTools/DbHandlers/MysqlHandler.cs
+++ SqlConvertTools/DbHandlers/MysqlHandler.cs
@@ -3,7 +3,6 @@ using System.Data;
 using System.Data.Common;
 using System.Diagnostics.CodeAnalysis;
 using MySql.Data.MySqlClient;
-using Newtonsoft.Json;
 using SqlConvertTools.Extensions;
 using SqlConvertTools.Helper;
 
