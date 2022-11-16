927d1a6f2bbb52ff5dc02cb486fbb28b4a80b4dd
miryo <miryo@vcup.moe>
2022-11-16 13:56:44 +0800

    ~

--- SqlConvertTools/Program.cs
+++ SqlConvertTools/Program.cs
@@ -3,9 +3,7 @@ using SqlConvertTools.Commands;
 
 var rootCommand = new RootCommand();
 
-rootCommand.AddCommand(new SqlServerToSqliteCommand());
 rootCommand.AddCommand(new SqlServerTransferCommand());
-rootCommand.AddCommand(new SqliteToSqlserverCommand());
 rootCommand.AddCommand(new SqlServerToMySqlCommand());
 
 return await rootCommand.InvokeAsync(args);
